e28a853a20c48ac00210f5ddbf245aaf573cdc14
NikitKhom <nikita.khomenko.n25@gmail.com>
2025-11-08 23:35:02 +0300

    feat/ add store

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -5,23 +5,40 @@ import RegistePage from '../../pages/RegistePage/RegistePage.vue'
 
 const routes = [
   {
-    path: '/',
+    path: '/dashboard/',
     name: 'MainPage',
     component: MainPage,
+    meta: { requiresAuth: true }
   },
   {
-    path: '/signin',
+    path: '/signin/',
     name: 'LoginPage',
     component: LoginPage,
+    meta: { requiresGuest: true }
   },
   {
-    path: '/signup',
+    path: '/signup/',
     name: 'RegistePage',
     component: RegistePage,
+    meta: { requiresGuest: true }
   },
-]
+];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const existingPage = to.matched.length > 0;
+  const isAuthenticated = true;
+
+ 
+  if (requiresAuth && !isAuthenticated) {
+    next('/signin/')
+  } else if (existingPage) {
+      next()
+    } else next('/dashboard/')
 })
+
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './app/styles/style.css'
 import App from './app/App.vue'
 import { router } from './app/router';
+import { createPinia } from 'pinia';
 
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
 app.use(router)
 app.mount('#app')
